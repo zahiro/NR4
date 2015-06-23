e8189bb86b5f56ead5fbfb32631b4ca48539d6e7
zahiro <zahiroprem@gmail.com>
2015-06-23 15:02:47 +0300

    Delete NR3_global.R

--- NR3_global.R
+++
@@ -1,5 +0,0 @@
-############################# g l o b a l . R
-
-geo.list <<- c( "BR"   ,   "CA"  ,    "EG"     , "ES"    ,  "FR"   ,   "GB"    ,  "IN"   ,   "AllGEOS" ,"RU" ,     "SA"  ,    "US")  #levels(mydata$country)
-device.list <<- c(""      ,      "MediaCenter" ,"AllDevices",  "PC"   ,       "Phone"   ,    "Tablet") ## levels(mydata$device_type)
-
